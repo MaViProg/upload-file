34859715333957e219f0f5b7490a30dc9ebaa4d7
Оганнисян Вардан <v.ogannisyan@razvitie-mo.ru>
2021-02-12 12:07:16 +0300

    swagger config

--- src/main/java/com/example/uploadfile/controller/UploadController.java
+++ src/main/java/com/example/uploadfile/controller/UploadController.java
@@ -4,6 +4,9 @@ package com.example.uploadfile.controller;
 
 import com.example.uploadfile.dto.UploadFileResponse;
 import com.example.uploadfile.service.UploadService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -22,6 +25,11 @@ public class UploadController {
     private UploadService uploadService;
 
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = String.class),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
+    @ApiOperation(value = "Загрузить файл")
     @PostMapping("/uploadFile")
     public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
 
